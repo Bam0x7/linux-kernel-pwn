c206aa4f1cc904e78def57447a96ed8fca5829d1
Bam0x7 <90852683+Bam0x7@users.noreply.github.com>
2024-08-06 09:02:13 +0700

    Add files via upload

---
+++ cache-of-castaways/compress.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+gcc -o exp exp.c -static $1
+mv ./exp ./initramfs
+cd initramfs
+find . -print0 \
+| cpio --null -ov --format=newc \
+| gzip -9 > initramfs.cpio.gz
+mv ./initramfs.cpio.gz ../
